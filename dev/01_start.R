3084d4bc772f2bb830c9def106ebfcd289ff7b2b
Chris Beeley <chris.beeley@gmail.com>
2021-08-11 19:53:52 +0100

    Initial commit

---
+++ dev/01_start.R
@@ -0,0 +1,70 @@
+# Building a Prod-Ready, Robust Shiny Application.
+# 
+# README: each step of the dev files is optional, and you don't have to 
+# fill every dev scripts before getting started. 
+# 01_start.R should be filled at start. 
+# 02_dev.R should be used to keep track of your development during the project.
+# 03_deploy.R should be used once you need to deploy your app.
+# 
+# 
+########################################
+#### CURRENT FILE: ON START SCRIPT #####
+########################################
+
+## Fill the DESCRIPTION ----
+## Add meta data about your application
+## 
+## /!\ Note: if you want to change the name of your app during development, 
+## either re-run this function, call golem::set_golem_name(), or don't forget
+## to change the name in the app_sys() function in app_config.R /!\
+## 
+golem::fill_desc(
+  pkg_name = "warfarin_doser", # The Name of the package containing the App 
+  pkg_title = "Store INR levels and communicate with the anticoagulation team", # The Title of the package containing the App 
+  pkg_description = "This package serves two functions. Firstly it stores the 
+  results of INR tests for individuals who take warfarin, and visualises them. 
+  Secondly, it automatically sends emails to the team containing name, NHS number 
+  and DOB. These can easily be configured per user in the YAML file. THIS 
+  APPLICATION PUTS YOUR MEDICAL INFORMATION WHEREVER YOU HOST IT. ONLY DO THIS IF 
+  YOU UNDERSTAND THE RISKS.", # The Description of the package containing the App 
+  author_first_name = "AUTHOR_FIRST", # Your First Name
+  author_last_name = "AUTHOR_LAST", # Your Last Name
+  author_email = "AUTHOR@MAIL.COM", # Your Email
+  repo_url = NULL # The URL of the GitHub Repo (optional) 
+)     
+
+## Set {golem} options ----
+golem::set_golem_options()
+
+## Create Common Files ----
+## See ?usethis for more information
+usethis::use_mit_license( "Golem User" )  # You can set another license here
+usethis::use_readme_rmd( open = FALSE )
+usethis::use_code_of_conduct()
+usethis::use_lifecycle_badge( "Experimental" )
+usethis::use_news_md( open = FALSE )
+
+## Use git ----
+usethis::use_git()
+
+## Init Testing Infrastructure ----
+## Create a template for tests
+golem::use_recommended_tests()
+
+## Use Recommended Packages ----
+golem::use_recommended_deps()
+
+## Favicon ----
+# If you want to change the favicon (default is golem's one)
+golem::use_favicon() # path = "path/to/ico". Can be an online file. 
+golem::remove_favicon()
+
+## Add helper functions ----
+golem::use_utils_ui()
+golem::use_utils_server()
+
+# You're now set! ----
+
+# go to dev/02_dev.R
+rstudioapi::navigateToFile( "dev/02_dev.R" )
+
